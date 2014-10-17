9192d00fa5bff606eb04dbb38eef6d1ef75253b2
douzmaniangit config --global user.name douzmanian <berapturous@gmail.com>
2014-10-17 12:05:56 +0200

    added local storage

--- spark.js
+++ spark.js
@@ -11,3 +11,24 @@ q = new TinderQuery();
 var ajaxButton = q.querySelector('#ajax');
 ajaxButton.on('click', makeAjaxCall);
 
+
+
+var saveToStorage = function(){
+	if (locationData.length === 0){
+		throw new Error("No location data to save.")
+	};
+	var arrayLength = locationData.length;
+	for (var i = 0; i < arrayLength; i++){
+		var key = locationData[i].name;
+		var locationInfo = locationData[i];
+		delete locationInfo["name"];
+		storageData.create(key, locationInfo);
+	};
+	saveButton.after("<span> Success!</span>");
+};
+
+
+var storageData = TinderStorage.getInstance();
+var saveButton = q.querySelector('#save');
+saveButton.on('click', saveToStorage);
+
