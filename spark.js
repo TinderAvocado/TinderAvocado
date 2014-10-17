89d907a712e9677f46bba984ba234a1f6da9fadb
Emily Chen <tenshiemi@gmail.com>
2014-10-17 11:13:51 +0200

    Changed object to array

--- spark.js
+++ spark.js
@@ -1,11 +1,11 @@
 var makeAjaxCall = function() {
 	TinderAjax.getJSON("http://ironhack-meet-me-api.herokuapp.com/locations.json", function(data) {
     ajaxButton.after("<span> Success!</span>");
-    locationData = data;
+    locationData = data.locations_list;
   });
 };
 
-var locationData = {};
+var locationData = [];
 
 q = new TinderQuery();
 var ajaxButton = q.querySelector('#ajax');
